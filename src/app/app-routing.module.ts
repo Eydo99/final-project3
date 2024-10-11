53005ab67e155eef47631bf3ab0eaf4dc995d33e
eyad amr <eyadamr1644@gmail.com>
2024-10-11 15:27:45 +0300

    first prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';  
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'services',component:ServicesComponent},
+ {path:'projects',component:ProjectsComponent},
+ {path:'blog',component:BlogComponent},
+ {path:'contact',component:ContactComponent},
+ {path:'project-details',component:ProjectDetailsComponent},
+ {path:'blog-details',component:BlogDetailsComponent},
+ {path:'service-details',component:ServiceDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ContactComponent,
+    ProjectDetailsComponent,
+    BlogDetailsComponent,
+    ServiceDetailsComponent,
+  
   ],
   imports: [
     BrowserModule,
